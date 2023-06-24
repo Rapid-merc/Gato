5a3e59feb38864441ca6f3a79c555a120c94b39a
subhamctc3007 <subhamctc3007@gmail.com>
2023-06-25 02:33:18 +0530

    Added a scrolling effect

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', ()=>{
+    let reveals = document.querySelectorAll('.reveal');
+    for(let i = 0; i < reveals.length; i++){
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+        if(revealTop < windowHeight-revealPoint) reveals[i].classList.add('active');
+        else reveals[i].classList.remove('active');
+    }
+});
