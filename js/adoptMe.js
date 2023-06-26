26f0f1a8684ec427aa5dc91d791644751590459c
subhamctc3007 <subhamctc3007@gmail.com>
2023-06-27 00:19:16 +0530

    adopt me renewed

--- js/adoptMe.js
+++ js/adoptMe.js
@@ -1,66 +1,76 @@
 const apiKey = 'live_15r9tqD7181iOJiLITeBYiW0WRVHunOVJLhxN73ylrqtmTKiCC52pl3jsGAqqS3n';
-const characteristicsInput = document.getElementById('characteristics');
+const characteristicInput = document.getElementById('characteristic');
 const findCatButton = document.getElementById('findCatButton');
-const catBreed = document.getElementById('catBreed');
-const catCharacteristics = document.getElementById('catCharacteristics');
-const catImage = document.getElementById('catImage');
 
 // Function to fetch cat data based on user characteristics
-async function fetchCatByCharacteristics(characteristics) {
-  const response = await fetch(`https://api.thecatapi.com/v1/breeds?api_key=${apiKey}`);
-  const data = await response.json();
+async function fetchCatsByCharacteristic(characteristic) {
+    const response = await fetch(`https://api.thecatapi.com/v1/breeds?api_key=${apiKey}`); // fetch data for all breeds
+    data = await response.json();
 
-  const matchedBreeds = data.filter(breed => {
-    const breedCharacteristics = breed.description.toLowerCase();
-    return characteristics.toLowerCase().split(',').every(char => breedCharacteristics.includes(char.trim()));
-  });
-
-  if (matchedBreeds.length > 0) {
-    const randomIndex = Math.floor(Math.random() * matchedBreeds.length);
-    return matchedBreeds[randomIndex];
-  } else {
-    return null;
-  }
+    // first, filter out those breeds which do not have image
+    data = data.filter(img => img.image?.url != null);
+    // filter out those breeds which have the selected characteristic
+    const matchedBreeds = data.filter(breed => {
+        // temperament property contains the list of characteristics of the breed, separated by commas
+        const listOfCharacteristics =  breed.temperament.split(', ');
+        if(listOfCharacteristics.includes(characteristic)) return breed;
+    });
+    return matchedBreeds;
 }
 
-// Function to display cat information
-function displayCatInfo(cat) {
-  if (cat) {
-    catBreed.textContent = cat.name;
-    catCharacteristics.textContent = cat.description;
-    catImage.src = `https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`;
-    catImage.alt = cat.name;
-    catImage.style.display = 'block';
-    catResultContainer.classList.add('special'); // Add the special class to the card
-  }
+async function displayInfo(cats){
+    console.log(cats);
+    
+    for(let id = 0; id < cats.length; id++){
+        // creating a card
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.setAttribute('id', `${id}`);
+        // Child elements
+        let h1 = document.createElement('h1');
+        h1.setAttribute('id', `breed${id}`);
+        h1.innerHTML = cats[id].name;
+        card.appendChild(h1);
 
-  else {
-    catBreed.textContent = 'No Match Found';
-    catCharacteristics.textContent = 'Sorry, we could not find a cat breed matching your characteristics.';
-    catImage.src = '';
-    catImage.alt = '';
-    catImage.style.display = 'none';
-    catResultContainer.classList.remove('special'); // Remove the special class from the card
-  }
+        let img = document.createElement('img');
+        img.setAttribute('id', `image${id}`);
+        img.setAttribute('src', cats[id].image.url);
+        card.appendChild(img);
+
+        let country = document.createElement('p');
+        country.setAttribute('id', `characteristics${id}`);
+        country.innerHTML = `Country of Origin: ${cats[id].origin}`;
+        card.appendChild(country);
+
+        let characteristics = document.createElement('p');
+        characteristics.setAttribute('id', `characteristics${id}`);
+        characteristics.innerHTML = cats[id].temperament;
+        card.appendChild(characteristics);
+
+        let description = document.createElement('p');
+        description.setAttribute('id', `description${id}`);
+        description.innerHTML = cats[id].description;
+        card.appendChild(description);
+
+        let wiki = document.createElement('a');
+        let btn = document.createElement('button');
+        btn.innerHTML = 'Learn More';
+        wiki.href = cats[id].wikipedia_url;
+        wiki.setAttribute('target', '_blank');
+        wiki.appendChild(btn);
+        card.appendChild(wiki);
+
+        document.getElementById('catResultContainer').appendChild(card);
+    }
 }
-
 
 // Event listener for the "Find My Cat" button
 findCatButton.addEventListener('click', async () => {
-  const characteristics = characteristicsInput.value.trim();
-  if (characteristics !== '') {
-    const cat = await fetchCatByCharacteristics(characteristics);
-    displayCatInfo(cat);
-
-    // const element = document.getElementById('catResultContainer');
-    // html2canvas(element).then(function(canvas){
-    //   document.getElementById('result').append(canvas);
-
-    //   let cvs = document.querySelector('canvas');
-    //   let downloadLink = document.getElementById('download-page-as-image');
-
-    //   downloadLink.href = cvs.toDataURL();
-    //   downloadLink.download = 'fileName.png';
-    // });
-  }
+    document.getElementById('catResultContainer').innerHTML = '';
+    const characteristic = characteristicInput.value;
+    if (characteristic !== '') {
+        const cats = await fetchCatsByCharacteristic(characteristic);
+        if(cats.length !== 0) displayInfo(cats);
+        else document.getElementById('sorry').classList.remove('hide');
+    }
 });
